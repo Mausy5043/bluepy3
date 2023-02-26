a54a4feb935ce6941e163f563fbe15463ed8b0d2
Maurice (mausy5043) Hendrix <Mausy5043@users.noreply.github.com>
2023-02-26 09:47:21 +0100

    for debugging purposes

--- src/bluepy3/get_services.py
+++ src/bluepy3/get_services.py
@@ -80,7 +80,10 @@ def get_table(url, local_filename, table_defs):
     """
     html = get_html(url, local_filename)
     for row in get_table_rows(html):
-        assert len(row) == len(table_defs)
+        if DEBUG:
+            print(f"got {row}")
+        try:
+            assert len(row) == len(table_defs)
 
         ret = {}
         for col, (name, func) in zip(row, table_defs):
@@ -100,6 +103,8 @@ def get_table(url, local_filename, table_defs):
 
 
 def get_service_names():
+    if DEBUG:
+        print("\nservices")
     for row in get_table(
         "https://web.archive.org/web/20160318231314/https://developer.bluetooth.org/gatt/services/Pages/ServicesHome.aspx",
         "services.html",
@@ -110,11 +115,17 @@ def get_service_names():
             ("Level", None),
         ),
     ):
+        if DEBUG:
+            # example:
+            # row {'Name': 'Weight Scale', 'Type': 'org.bluetooth.service.weight_scale', 'Number': 6173, 'Level': 'Adopted'}
+            print(f"row {row}")
         row["cname"] = row["Type"].split(".")[-1]
         yield row
 
 
 def get_descriptors():
+    if DEBUG:
+        print("\ndescriptors")
     for row in get_table(
         "https://web.archive.org/web/20160318161646/https://developer.bluetooth.org/gatt/descriptors/Pages/DescriptorsHomePage.aspx",
         "descriptors.html",
@@ -125,11 +136,17 @@ def get_descriptors():
             ("Level", None),
         ),
     ):
+        if DEBUG:
+            # example
+            # row {'Name': 'Value Trigger Setting', 'Type': 'org.bluetooth.descriptor.value_trigger_setting', 'Number': 10506, 'Level': 'Adopted'}
+            print(f"row {row}")
         row["cname"] = row["Type"].split(".")[-1]
         yield row
 
 
 def get_characteristics():
+    if DEBUG:
+        print("\ncharacteristics")
     for row in get_table(
         "https://web.archive.org/web/20160318225546/https://developer.bluetooth.org/gatt/characteristics/Pages/CharacteristicsHome.aspx",
         "characteristics.html",
@@ -140,26 +157,42 @@ def get_characteristics():
             ("Level", None),
         ),
     ):
+        if DEBUG:
+            # example:
+            # row {'Name': 'Wind Chill', 'Type': 'org.bluetooth.characteristic.wind_chill', 'Number': 10873, 'Level': 'Adopted'}
+            print(f"row {row}")
         row["cname"] = row["Type"].split(".")[-1]
         yield row
 
 
 def get_units():
+    if DEBUG:
+        print("\nunits")
     for row in get_table(
         "https://web.archive.org/web/20160305020847/https://developer.bluetooth.org/gatt/units/Pages/default.aspx",
         "units.html",
         (("Number", lambda x: int(x, 16)), ("Name", None), ("Type", None)),
     ):
+        if DEBUG:
+            # example:
+            # {'Number': 10168, 'Name': 'mass (pound)', 'Type': 'org.bluetooth.unit.mass.pound'}
+            print(row)
         row["cname"] = row["Type"].split(".")[-1]
         yield row
 
 
 def get_formats():
+    if DEBUG:
+        print("\nformats")
     for row in get_table(
         "https://web.archive.org/web/20160410055350/https://developer.bluetooth.org/gatt/Pages/FormatTypes.aspx",
         "formats.html",
         (("Name", None), ("Description", None)),
     ):
+        if DEBUG:
+            # example:
+            # row {'Name': 'float64', 'Description': 'IEEE-754 64-bit floating point'}
+            print(f"row {row}")
         row["cname"] = row["Name"]
         yield row
 
@@ -213,6 +246,23 @@ class Definitions(object):
             "descriptor_UUIDs": [(row["Number"], row["cname"], row["Name"]) for row in self.descriptors],
             "units_UUIDs": [(row["Number"], row["cname"], row["Name"]) for row in self.units],
             "formats": [(row["Name"], row["Description"]) for row in self.formats],
+        }
+
+
+    def data_debug(self):
+        """ FOR DEVBUGGING ONLY
+        Makes tables like this:
+        number, name, common name.
+        """
+        return {
+            # Units = OK
+            # "units_UUIDs": [(row["Number"], row["cname"], row["Name"]) for row in self.units],
+            # Formats = OK
+            # "formats": [(row["Name"], row["Description"]) for row in self.formats],
+            # Characteristics = OK
+            # "characteristic_UUIDs": [(row["Number"], row["cname"], row["Name"]) for row in self.characteristics],
+            # "service_UUIDs": [(row["Number"], row["cname"], row["Name"]) for row in self.services],
+            "descriptor_UUIDs": [(row["Number"], row["cname"], row["Name"]) for row in self.descriptors],
         }
 
 
