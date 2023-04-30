da05e7ab6603efaa4de7b26033ca2e5cd6d3f0c2
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2023-04-30 10:40:57 +0200

    version bump (production)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import sys
 from setuptools import setup
 from setuptools.command.build_py import build_py
 
-VERSION = "1.3.2"
+VERSION = "1.4.1"
 MAKEFILE = "bluepy3/Makefile"
 VERSION_FILE = "bluepy3/version.h"
 BLUEZ_VERSION = "(unknown)"
