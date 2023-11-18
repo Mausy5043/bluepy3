45e7328e09a0198a2266e16183ee6bc6f36d4178
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2023-11-18 10:07:14 +0100

    sometimes `from . import` doesn't work

--- src/bluepy3/__init__.py
+++ src/bluepy3/__init__.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
-from . import btle
+try:
+    from . import btle
+    from . import helpermaker
+except ImportError:
+    import btle
+    import helpermaker
 
-__all__: list[str] = ["btle"]
+__all__: list[str] = ["btle", "helpermaker"]
