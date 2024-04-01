1dc5bc4fdb3c7a8890115459a19747e17eb716d9
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2024-04-01 09:15:51 +0200

    add type

--- bluepy3/helpermaker.py
+++ bluepy3/helpermaker.py
@@ -139,7 +139,7 @@ def build_helper() -> None:
     with open(MAKEFILE, "w", encoding="utf-8") as makefile:
         for line in lines:
             if line.startswith("BLUEZ_VERSION"):
-                line = f"BLUEZ_VERSION={BLUEZ_VERSION}\n"
+                line: str = f"BLUEZ_VERSION={BLUEZ_VERSION}\n"
             makefile.write(line)
     if platform.system().lower() == "linux":
         # Windows and macOS are not supported
